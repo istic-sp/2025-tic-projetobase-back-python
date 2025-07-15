0c8fa60b24b62eaffc3e23d31c0ad409b12077aa
DiogoBotton <diogo.luo@gmail.com>
2025-07-15 15:16:00 -0300

    feat: edit and delete endpoints, healthcheck in db in docker compose

---
+++ fast_api/app/src/infrastructure/validations/existence.py
@@ -0,0 +1,20 @@
+from sqlalchemy.orm import Session
+from sqlalchemy.sql import exists
+from uuid import UUID
+from fastapi.exceptions import RequestValidationError
+from src.core.abstractions.domain_base import DomainBase
+
+def entity_id_exists(db: Session, entity: DomainBase, id: UUID):
+    entity = (db
+              .query(exists()
+                     .where(entity.id == id and entity.deleted_at.is_(None)))
+              .scalar())
+    
+    return entity
+
+def field_error(field_name: str, message: str):
+    return RequestValidationError(errors=[{
+        "loc": ("body", field_name),
+        "msg": message,
+        "type": "value_error"
+    }])
